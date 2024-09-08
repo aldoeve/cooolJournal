71e32a7ef4725629980f9b506ecfaf7939bd43ea
aldo <aldo@cool.com>
2024-09-07 19:30:07 -0500

    added lion pic and contributions

---
+++ CREDITS.md
@@ -0,0 +1,2 @@
+<center>We do not claim all the images in this repo as ours. Below is the credit for what does not belong to us as its creators.
+</center>
--- webapp/src/components/Profile.vue
+++ webapp/src/components/Profile.vue
@@ -5,4 +5,21 @@ import { useRouter } from 'vue-router';
 
 <template>
 
+
 </template>
+
+<style>
+.temp {
+    background-color: white;
+}
+.profileRow{
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+}
+.profileCol{
+    display:flex;
+    flex-direction: column;
+    flex:1;
+}
+</style>
