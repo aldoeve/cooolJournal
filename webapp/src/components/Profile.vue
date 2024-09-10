f20e46564ddd09550378f7155ebb3c3b06f7d3b8
aldo <aldo@cool.com>
2024-09-09 23:58:33 -0500

    fixed the first rows button and image position

--- webapp/src/components/Profile.vue
+++ webapp/src/components/Profile.vue
@@ -1,25 +1,46 @@
 <script setup>
-import { ref } from 'vue';
-import { useRouter } from 'vue-router';
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 </script>
 
 <template>
-
-
+  <div class="profileRow">
+    <div class="profileCol">
+      <div class="circleBase circleImage">
+        <img src="../../LionProfilePic.jpg" />
+      </div>
+      <button class="profileCol saveButton"><span>Save</span></button>
+    </div>
+  </div>
+  <div class="profileRow"></div>
+  <div class="profileRow"></div>
 </template>
 
 <style>
-.temp {
-    background-color: white;
+.circleBase {
+  background-color: white;
+  border-radius: 50%;
+  width: 40vh;
+  height: 40vh;
 }
-.profileRow{
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
+.userName {
+  background-color: white;
+  width: 40vh;
+  height: 40vh;
 }
-.profileCol{
-    display:flex;
-    flex-direction: column;
-    flex:1;
+.saveButton {
+  background-color: white;
+  size: 1vh;
+}
+.profileRow {
+  display: flex;
+}
+.profileCol {
+  flex: 50%;
+}
+.saveButtonPosition {
+  position: inherit;
+  bottom: 0;
+  left: 0;
 }
 </style>
