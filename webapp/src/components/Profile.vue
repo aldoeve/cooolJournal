654415a1cc1feb56e5ae0676f18777eb7c6a2653
aldo <aldo@cool.com>
2024-09-10 19:19:18 -0500

    it looks closer to what it needs to be

--- webapp/src/components/Profile.vue
+++ webapp/src/components/Profile.vue
@@ -1,6 +1,12 @@
 <script setup>
 import { ref } from "vue";
 import { useRouter } from "vue-router";
+
+const changePicture = ref(false);
+
+function requestProfileChange() {
+  changePicture != changePicture;
+}
 </script>
 
 <template>
@@ -9,11 +15,30 @@ import { useRouter } from "vue-router";
       <div class="circleBase circleImage">
         <img src="../../LionProfilePic.jpg" />
       </div>
-      <button class="profileCol saveButton"><span>Save</span></button>
+      <button
+        class="profileCol saveButton saveButtonPosition"
+        @click="requestProfileChange"
+      >
+        <span>Change</span>
+      </button>
     </div>
   </div>
-  <div class="profileRow"></div>
-  <div class="profileRow"></div>
+  <div class="profileRow">
+    <div class="profileCol">
+      <div class="username"><span>User</span></div>
+    </div>
+    <button class="profileCol saveButton">
+      <span>Change</span>
+    </button>
+  </div>
+  <div class="profileRow">
+    <div class="profileCol">
+      <div class="username bio"><span>Hello, I am here to have fun.</span></div>
+    </div>
+    <button class="profileCol saveButton">
+      <span>Change</span>
+    </button>
+  </div>
 </template>
 
 <style>
@@ -23,24 +48,34 @@ import { useRouter } from "vue-router";
   width: 40vh;
   height: 40vh;
 }
-.userName {
+.username {
   background-color: white;
+  color: black;
   width: 40vh;
-  height: 40vh;
+  height: 4vh;
+  text-align: center;
+  font-size: large;
+  font-weight: bold;
+  overflow: hidden;
+}
+.bio {
+  width: 40vh;
+  height: 20vh;
 }
 .saveButton {
   background-color: white;
   size: 1vh;
+  height: 4vh;
+  text-align: center;
+  font-size: large;
+  font-weight: bold;
 }
 .profileRow {
   display: flex;
+  margin-bottom: 10vh;
 }
 .profileCol {
   flex: 50%;
-}
-.saveButtonPosition {
-  position: inherit;
-  bottom: 0;
-  left: 0;
+  margin-left: 5vh;
 }
 </style>
