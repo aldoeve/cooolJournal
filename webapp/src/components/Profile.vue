1a03dc458087819bf20db8840f797fb28afeb65b
aldo <aldo@cool.com>
2024-09-11 15:06:51 -0500

    The page looks like how we want it to look now it needs to be reactive

--- webapp/src/components/Profile.vue
+++ webapp/src/components/Profile.vue
@@ -10,38 +10,37 @@ function requestProfileChange() {
 </script>
 
 <template>
-  <div class="profileRow">
-    <div class="profileCol">
-      <div class="circleBase circleImage">
-        <img src="../../LionProfilePic.jpg" />
-      </div>
-      <button
-        class="profileCol saveButton saveButtonPosition"
-        @click="requestProfileChange"
-      >
-        <span>Change</span>
-      </button>
+  <div class="profileLayout">
+    <div class="circleBase circleImage imgPosition">
+      <img src="../../LionProfilePic.jpg" />
     </div>
-  </div>
-  <div class="profileRow">
-    <div class="profileCol">
-      <div class="username"><span>User</span></div>
-    </div>
-    <button class="profileCol saveButton">
+    <button class="saveButton imgButtonPosition" @click="requestProfileChange">
       <span>Change</span>
     </button>
-  </div>
-  <div class="profileRow">
-    <div class="profileCol">
-      <div class="username bio"><span>Hello, I am here to have fun.</span></div>
+    <div class="username usernamePosition"><span>User</span></div>
+    <button class="saveButton usernameButtonPosition">
+      <span>Change</span>
+    </button>
+    <div class="username bio bioPosition">
+      <span>Hello, I am here to have fun ah ha ha.</span>
     </div>
-    <button class="profileCol saveButton">
+    <button class="saveButton bioButtonPosition">
       <span>Change</span>
     </button>
   </div>
 </template>
 
 <style>
+.profileLayout {
+  display: grid;
+  grid-template-columns: 50% 20%;
+  grid-template-rows: 50% 50% 50%;
+  gap: 5vh;
+  place-self: center;
+  justify-content: center;
+  position: relative;
+  row-gap: 5vh;
+}
 .circleBase {
   background-color: white;
   border-radius: 50%;
@@ -57,25 +56,43 @@ function requestProfileChange() {
   font-size: large;
   font-weight: bold;
   overflow: hidden;
+  grid-area: 3/1;
 }
 .bio {
   width: 40vh;
   height: 20vh;
+  text-align: left;
+  padding-left: 1vh;
 }
 .saveButton {
   background-color: white;
   size: 1vh;
+  width: 11vh;
   height: 4vh;
   text-align: center;
   font-size: large;
   font-weight: bold;
 }
-.profileRow {
-  display: flex;
-  margin-bottom: 10vh;
+.imgPosition {
+  grid-area: 1/1;
 }
-.profileCol {
-  flex: 50%;
-  margin-left: 5vh;
+.imgButtonPosition {
+  margin-top: 36vh;
+  grid-area: 1/2;
+}
+.usernamePosition {
+  grid-area: 2/1;
+}
+.usernameButtonPosition {
+  grid-area: 2/2;
+  margin-left: 8vh;
+}
+.bioPosition {
+  grid-area: 3/1;
+}
+.bioButtonPosition {
+  grid-area: 3/2;
+  margin-left: 8vh;
+  margin-top: 6vh;
 }
 </style>
