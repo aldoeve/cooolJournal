032714e48e31b3567f2d53182953cb4c14dd18d4
aldo <aldo@cool.com>
2024-09-24 21:30:26 -0500

    restarted profile

--- webapp/src/views/Profile.vue
+++ webapp/src/views/Profile.vue
@@ -1,109 +1,49 @@
 <script setup>
 import { ref } from "vue";
 import { useRouter } from "vue-router";
+import "../assets/welcome.css";
+import "../assets/profile.css";
+import "../assets/util.css";
+
+const router = useRouter();
 
 const changePicture = ref(false);
 
 function requestProfileChange() {
   changePicture != changePicture;
 }
+
+function gotoHome() {
+  router.push("/home");
+}
 </script>
 
+<style scoped>
+@import "../assets/top_bar.css";
+@import "../assets/font-awesome-4.7.0/css/font-awesome.min.css";
+@import "../assets/welcome.css";
+</style>
+
 <template>
-  <div class="profileLayout">
-    <div class="circleBase circleImage imgPosition">
-      <img src="../../LionProfilePic.jpg" />
+  <div class="wrapper">
+    <div class="profileLayout">
+      <div class="circleBase circleImage imgPosition">
+        <img src="../../LionProfilePic.jpg" />
+      </div>
+      <button @click="requestProfileChange"><span>Change</span></button>
+      <!--<button class="saveButton imgButtonPosition" @click="requestProfileChange">
+        <span>Change</span>
+      </button>
+      <div class="username usernamePosition"><span>User</span></div>
+      <button class="saveButton usernameButtonPosition">
+        <span>Change</span>
+      </button>
+      <div class="username bio bioPosition">
+        <span>Hello, I am here to have fun ah ha ha.</span>
+      </div>
+      <button class="saveButton bioButtonPosition">
+        <span>Change</span>
+      </button>-->
     </div>
-    <button class="saveButton imgButtonPosition" @click="requestProfileChange">
-      <span>Change</span>
-    </button>
-    <div class="username usernamePosition"><span>User</span></div>
-    <button class="saveButton usernameButtonPosition">
-      <span>Change</span>
-    </button>
-    <div class="username bio bioPosition">
-      <span>Hello, I am here to have fun ah ha ha.</span>
-    </div>
-    <button class="saveButton bioButtonPosition">
-      <span>Change</span>
-    </button>
   </div>
 </template>
-
-<style>
-.profileLayout {
-  display: grid;
-  grid-template-columns: 50% 20%;
-  grid-template-rows: 50% 50% 50%;
-  gap: 5vh;
-  place-self: center;
-  justify-content: center;
-  position: relative;
-  row-gap: 5vh;
-}
-.circleBase {
-  background-color: white;
-  border-radius: 50%;
-  width: 40vh;
-  height: 40vh;
-}
-.username {
-  background-color: white;
-  color: black;
-  width: 40vh;
-  height: 4vh;
-  text-align: center;
-  font-size: 2.8vh;
-  font-weight: bold;
-  overflow: hidden;
-}
-.bio {
-  width: 40vh;
-  height: 20vh;
-  text-align: left;
-  padding-left: 1vh;
-}
-.saveButton {
-  display: flex;
-  background-color: white;
-  size: 1vh;
-  width: 11vh;
-  height: 4vh;
-  text-align: left;
-  font-size: 2.8vh;
-  font-weight: bold;
-}
-.imgPosition {
-  grid-area: 1/1;
-}
-.imgButtonPosition {
-  margin-top: 36vh;
-  grid-area: 1/2;
-}
-.usernamePosition {
-  grid-area: 2/1;
-}
-.usernameButtonPosition {
-  grid-area: 2/2;
-  margin-left: 8vh;
-}
-.bioPosition {
-  grid-area: 3/1;
-}
-.bioButtonPosition {
-  grid-area: 3/2;
-  margin-left: 8vh;
-  margin-top: 6vh;
-}
-@media only screen and ((max-width: 500px) or (max-height: 250px)) {
-  .saveButton {
-    display: none;
-  }
-  .bio {
-    display: none;
-  }
-  .username {
-    display: none;
-  }
-}
-</style>
