a0317f977f12c2cdcd50835fbe771df173fb1f72
JeremyPalomo <jeremypalomo02@gmail.com>
2024-10-08 17:48:58 -0500

    Added function to create,reset database, and enter schema.

--- backend/server/main.go
+++ backend/server/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"cooolJournal/backend/setup"
 )
 
 func main() {
@@ -11,10 +12,17 @@ func main() {
 	server := http.FileServer(http.Dir("./../../frontend/dist"))
 	http.Handle("/", server)
 
+	//Handle Setup
+	flags := make(map[string]bool)
+	setup.SetupFlags(flags)
+	setup.SetupDefault(flags)
+
 	//Starting the server.
 	fmt.Println("Server listening on :8080")
 	log.Panic(
 		http.ListenAndServe(":8080", nil),
 	)
+
+	
 
 }
