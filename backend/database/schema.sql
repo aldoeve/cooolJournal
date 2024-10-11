89e0ae7bb7025a951b13262c12d331e57cd969c6
JeremyPalomo <jeremypalomo02@gmail.com>
2024-10-10 19:23:24 -0500

    Created function to parse demo json data into the database.

--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,5 +1,37 @@
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+CREATE TABLE users (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
     email TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL
+    username VARCHAR(20) NOT NULL,
+    password TEXT NOT NULL UNIQUE,
+    terms date NOT NULL,
+    bio VARCHAR(300)
+);
+
+CREATE TABLE avatar (
+    avatar_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    avatar TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE topics(
+    topic_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    topic_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE journals(
+    journal_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    topic_id INTEGER NOT NULL,
+    journal_name VARCHAR(20) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (topic_id) REFERENCES topics(topic_id) ON DELETE CASCADE
+);
+
+CREATE TABLE pages(
+    page_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    journal_id INTEGER NOT NULL,
+    page_entry VARCHAR(500),
+    page_date date NOT NULL,
+    FOREIGN KEY (journal_id) REFERENCES journals(journal_id) ON DELETE CASCADE
 );
--- backend/setup/setup.go
+++ backend/setup/setup.go
@@ -7,14 +7,19 @@ import (
     _ "github.com/mattn/go-sqlite3"
     "os"
     "path/filepath"
+    "encoding/json"
+    "io/ioutil"
 )
 
 var(
 folderPath = "../database/"
 fileName = "database.db"
 schemaName = "schema.sql"
+demoDataName = "demo.json" 
 filePath = filepath.Join(folderPath, fileName)
 schemaPath = filepath.Join(folderPath, schemaName)
+demoDataPath = filepath.Join(folderPath,demoDataName)
+
 )
 
 
@@ -33,6 +38,7 @@ func SetupDefault(flags map[string]bool) {
         Createdb()
     }
 
+    LoadDemoData()
 }
 
 func Resetdb() {
@@ -74,11 +80,107 @@ func Createdb() {
     _, err = db.Exec(string(fileContents))
 
     if err != nil {
-		fmt.Println("Database schema entered unsuccessfully...")
+		fmt.Println("*Database schema entered unsuccessfully...*")
         panic(err)
 	} else {
 		fmt.Println("Database schema entered successfully...")
 	}
 
     defer db.Close()
+}
+
+func LoadDemoData() {
+    // Open the JSON file
+    jsonFile, err := os.Open(demoDataPath)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println("Successfully opened demo JSON file...")
+    defer jsonFile.Close()
+
+    //Read the JSON file
+    byteValue, _ := ioutil.ReadAll(jsonFile)
+
+    var result struct {
+        Users   []struct {
+            Email    string `json:"email"`
+            Username string `json:"username"`
+            Password string `json:"password"`
+            Terms    string `json:"terms"`
+            Bio      string `json:"bio"`
+        } `json:"users"`
+        Avatar  []struct {
+            UserID int    `json:"user_id"`
+            Avatar string `json:"avatar"`
+        } `json:"avatar"`
+        Topics []struct {
+            TopicName string `json:"topic_name"`
+        } `json:"topics"`
+        Journals []struct {
+            UserID  int `json:"user_id"`
+            TopicID int `json:"topic_id"`
+            JournalName string `json:"journal_name"`
+        } `json:"journals"`
+        Pages []struct {
+            JournalID int    `json:"journal_id"`
+            PageEntry string `json:"page_entry"`
+            PageDate  string `json:"page_date"`
+        } `json:"pages"`
+    }
+
+    //Parse json data
+    err = json.Unmarshal(byteValue, &result)
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Print("opening database...\n")
+    db, err := sql.Open("sqlite3", filePath)
+    if err != nil {
+        panic(err)
+    }
+    defer db.Close()
+
+    // Enters data into database
+    for _, user := range result.Users {
+        _, err := db.Exec("INSERT INTO users (email, username, password, terms, bio) VALUES (?, ?, ?, ?, ?)",
+            user.Email, user.Username, user.Password, user.Terms, user.Bio)
+        if err != nil {
+            fmt.Println("*user data *not* entered*")
+        }
+    }
+
+    for _, avatar := range result.Avatar {
+        _, err := db.Exec("INSERT INTO avatar (user_id, avatar) VALUES (?, ?)",
+            avatar.UserID, avatar.Avatar)
+        if err != nil {
+            fmt.Println("*avatar data *not* entered*")
+        }
+    }
+
+    for _, topic := range result.Topics {
+        _, err := db.Exec("INSERT INTO topics (topic_name) VALUES (?)",
+            topic.TopicName)
+        if err != nil {
+            fmt.Println("*topic data *not* entered*")
+        }
+    }
+
+    for _, journal := range result.Journals {
+        _, err := db.Exec("INSERT INTO journals (user_id, topic_id, journal_name) VALUES (?, ?, ?)",
+            journal.UserID, journal.TopicID, journal.JournalName)
+        if err != nil {
+            fmt.Println("*journal data *not* entered*")
+        }
+    }
+
+    for _, page := range result.Pages {
+        _, err := db.Exec("INSERT INTO pages (journal_id, page_entry, page_date) VALUES (?, ?, ?)",
+            page.JournalID, page.PageEntry, page.PageDate)
+        if err != nil {
+            fmt.Println("*page data *not* entered*")
+        }
+    }
+    fmt.Println("Entered correctly formated data into database...")
+
 }
